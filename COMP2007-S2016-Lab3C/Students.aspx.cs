91731d7123295e24d5b6bc7175ce05ddb4ba0029
Bloodham <bloodham25@gmail.com>
2016-06-02 19:00:12 -0400

    Added...Everything

---
+++ COMP2007-S2016-Lab3C/Students.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//using statements that are required to connect to the EF database
+using COMP2007_S2016_Lab3C.Models;
+using System.Web.ModelBinding;
+
+namespace COMP2007_S2016_Lab3C
+{
+    public partial class Students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if loading the page for the first time, populate the student grid
+            if(!IsPostBack)
+            {
+                //get the student data
+                this.GetStudents();
+            }
+        }
+
+        /**
+         * <summary>
+         * This method gets the student data from the database
+         * </summary>
+         * @method GetStudents
+         * @returns void
+         */
+        protected void GetStudents()
+        {
+            //Connect to the EF
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //query the Students table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+
+                //bind the result to the GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
+        }
+    }
+}
